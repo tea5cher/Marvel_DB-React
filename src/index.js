d427f1357373afc30acf4fad4fc04e440defde26
Egor Shkurov <grafushka10@mail.ru>
2022-03-11 12:01:32 +0300

    HTML/Style done

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-
+import App from './components/app/App';
+import './style/style.scss';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -10,5 +9,4 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
 
