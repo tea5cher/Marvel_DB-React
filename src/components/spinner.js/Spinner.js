1c04c37fd2ccd33d1a5b1fa5a90d23a4b9c4836c
Egor Shkurov <grafushka10@mail.ru>
2022-03-11 17:36:38 +0300

    some API functions

---
+++ src/components/spinner.js/Spinner.js
@@ -0,0 +1,8 @@
+const Spinner = () => {
+    return (
+        <svg xmlns:svg="http://www.w3.org/2000/svg" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.0" width="200px" height="200px" viewBox="0 0 128 128" xml:space="preserve"><rect x="0" y="0" width="100%" height="100%" fill="#FFFFFF" /><g><path fill="#e4b8bd" d="M99.359,10.919a60.763,60.763,0,1,0,0,106.162A63.751,63.751,0,1,1,99.359,10.919Z"/><animateTransform attributeName="transform" type="rotate" from="0 64 64" to="360 64 64" dur="1080ms" repeatCount="indefinite"></animateTransform></g><g><path fill="#c76a75" d="M28.641,117.081a60.763,60.763,0,1,0,0-106.162A63.751,63.751,0,1,1,28.641,117.081Z"/><animateTransform attributeName="transform" type="rotate" from="0 64 64" to="360 64 64" dur="1620ms" repeatCount="indefinite"></animateTransform></g><g><path fill="#9f0013" d="M117.081,99.313a60.763,60.763,0,1,0-106.162,0A63.751,63.751,0,1,1,117.081,99.313Z"/><animateTransform attributeName="transform" type="rotate" from="0 64 64" to="360 64 64" dur="3240ms" repeatCount="indefinite"></animateTransform></g></svg>
+    )
+}
+
+
+export default Spinner;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/App';
+import MarvelService from './services/MarvelService';
+
 import './style/style.scss';
+
 
 ReactDOM.render(
   <React.StrictMode>
